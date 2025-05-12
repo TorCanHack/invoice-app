6f3c76ab824c8b0d3f0391c48c94192bb10052e4
torcanhack <tor409617@gmail.com>
2025-05-12 10:46:03 +0100

    handled cors issues

--- server/server.js
+++ server/server.js
@@ -6,12 +6,17 @@ const dotenv = require('dotenv');
 const app = express();
 
 app.use(cors());
+const corsOptions = {
+  origin: "*", // or "*" for all origins
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"],
+};
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 dotenv.config();
 
 app.use('/api', invoiceRoutes);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
 
